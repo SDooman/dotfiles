be25cf33e8022eb10af1dac27af3c20c77ca3b33
Sam Dooman <samuel.dooman@gmail.com>
2024-05-30 20:43:16 -0400

    try this

---
+++ setup.sh
@@ -0,0 +1,138 @@
+###########################
+# This script installs software necessary
+# for Down Dog development.
+
+# Script borrows heavily from Adam Eivy's
+# automatic dotfiles.
+###########################
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+function ok() {
+    echo -e "$COL_GREEN[ok]$COL_RESET "$1
+}
+
+function bot() {
+    echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
+}
+
+function running() {
+    echo -en "$COL_YELLOW ⇒ $COL_RESET"$1": "
+}
+
+function action() {
+    echo -e "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
+}
+
+function warn() {
+    echo -e "$COL_YELLOW[warning]$COL_RESET "$1
+}
+
+function error() {
+    echo -e "$COL_RED[error]$COL_RESET "$1
+}
+
+function require_brew() {
+    running "brew $1 $2"
+    brew list $1 > /dev/null 2>&1 | true
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        action "brew install $1 $2"
+        brew install $1 $2
+        if [[ $? != 0 ]]; then
+            error "failed to install $1! aborting..."
+            # exit -1
+        fi
+    fi
+    ok
+}
+
+################
+# Begin Script #
+################
+
+bot "Hi! I'm going to install the software you need to work here! Here I go..."
+
+# Ask for the administrator password upfront
+if ! sudo grep -q "%wheel		ALL=(ALL) NOPASSWD: ALL #atomantic/dotfiles" "/etc/sudoers"; then
+
+  # Ask for the administrator password upfront
+  bot "I need you to enter your sudo password so I can install some things:"
+  sudo -v
+
+  # Keep-alive: update existing sudo time stamp until the script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+fi
+
+# Check if path already contains /usr/local/bin
+
+if [[ ":$PATH:" == *":/usr/local/bin:"* ]]; then
+	ok
+else
+	action "Modifying path to include /usr/local/bin"
+	echo '/usr/local/bin' | cat - /etc/paths > temp && mv temp /etc/paths
+fi
+
+running "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew, script $0 abort!"
+      exit 2
+    else
+      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+else
+  ok
+  # Make sure we’re using the latest Homebrew
+  running "updating homebrew"
+  brew update
+  ok
+  # Upgrade any already-installed formulae
+  action "upgrade brew packages..."
+  brew upgrade
+  ok "brews updated..."
+fi
+
+###################################
+# install brew cask (UI Packages) #
+###################################
+
+bot "Installing cask"
+brew tap homebrew/cask
+
+#####################
+# Homebrew Installs #
+#####################
+
+bot "Installing Brew Utilities"
+
+require_brew git
+require_brew gradle
+require_brew wget
+require_brew ffmpeg
+require_brew awscli
+
+#################
+# Cask Installs #
+#################
+
+bot "Installing Cask Utilities"
+
+require_brew figma
+require_brew github
+require_brew google-chrome
+require_brew intellij-idea
+require_brew sublime-text
+
